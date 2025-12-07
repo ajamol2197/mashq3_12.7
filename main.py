4b4a6ab84bbffed361e58a2284f118308e4e1105
ajamol2197 <ajamol2197@gmail.com>
2025-12-07 15:25:42 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# 1
+a = int(input("xarid summa kirit: "))
+if a >= 1.000:
+    print("20% chegirma bor")
+elif a >= 500:
+    print("10% chegirma bor")
+elif a >= 100:
+    print("5% chegirma bor")
+elif a < 100:
+    print("chegirma yoq")
+
+# 2
+a = int(input("vaqt kirit: "))
+if a >= 6:
+    print("kofe iching")
+elif a >= 12:
+    print("tushlik vaqti")
+elif a >= 18:
+    print("kechki ovqat, dam olish vaqti")
+elif 23 > a >6:
+    print("uxlash vaqti")
+
+# 3
+a = int(input("tel foizini kirit: "))
+if a >= 80:
+    print("batarya yaxshi holatda")
+elif a >= 50:
+    print("batarya ortacha")
+elif a >=20:
+    print("batarya kam")
+elif a < 20:
+    print("batarya juda kam")
